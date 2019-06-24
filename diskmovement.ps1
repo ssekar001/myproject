5372373e6ac66e8aaa21475492ba4442b85634fb
ssekar001 <41949887+ssekar001@users.noreply.github.com>
2019-06-24 16:53:00 +0530

    Add files via upload

---
+++ diskmovement.ps1
@@ -0,0 +1,13 @@
+﻿
+$FileContent = Import-Csv ".\disk1.csv"
+
+foreach($item in $FileContent)
+{
+  $VmName = $item.VMName
+  $ResourceGroupName= Get-AzureRmResource -Name $VmName | select ResourceGroupName -ExpandProperty ResourceGroupName
+  $DiskResourceId = $item.Id
+  
+Move-AzureRmResource -DestinationResourceGroupName $ResourceGroupName -ResourceId $DiskResourceId -Force
+
+}
+
